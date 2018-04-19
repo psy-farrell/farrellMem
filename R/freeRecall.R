6c21e25a266414a3d6bf5dacc77178c930a9fc12
Simon Farrell <simon.farrell@uwa.edu.au>
2018-04-19 15:48:23 +0800

    fix to getlagCRP to make sure omissions are not treated as legit responses

--- R/freeRecall.R
+++ R/freeRecall.R
@@ -53,7 +53,8 @@ getlagCRP <- function (indat, ll, doGroup=NULL, posStruct){
   # needs trial, outpos (cannot be a factor), serpos (cannot be a factor)
   # optional: gpos (not a factor)
 
-  # only pass in obtained responses (ie don;t pass in filled responses, where recalled==0)
+  indat <- indat[indat$recalled!=0,] # filter out omissions here--don't rely on user to do this
+
   # the function checks for repetitions CHECK
 
   numer <- rep(0,ll*2-1)
@@ -142,6 +143,64 @@ getlagCRP <- function (indat, ll, doGroup=NULL, posStruct){
                     lagrecw=numerw/denomw,
                     lagrecb=numerb/denomb))
 }
+
+# getlagCRPbySP <- function (indat, ll){
+#
+#   # note change in gpos name to be consistent with serpos
+#   # needs trial, outpos (cannot be a factor), serpos (cannot be a factor)
+#   # optional: gpos (not a factor)
+#
+#   # only pass in obtained responses (ie don;t pass in filled responses, where recalled==0)
+#   # the function checks for repetitions CHECK
+#
+#   numer <- matrix(0,ll,ll*2-1)
+#   denom <- numer
+#
+#   ll1 <- ll - 1
+#
+#   trials <- unique(indat$trial)
+#
+#   for (trial in trials){
+#
+#     tdat <- indat[indat$trial==trial,]
+#     tdat <- tdat[order(tdat$outpos),]
+#
+#     inseq <- tdat$serpos
+#
+#     if (!all(diff(tdat$outpos) == 1)){
+#       warning("Output positions not sequential on a trial")
+#     }
+#
+#     if (length(inseq)>1){
+#
+#       pool = setdiff(1:ll, inseq[1])
+#       prevWasRep <- FALSE
+#
+#       for (i in 2:length(inseq)){
+#         if (any(pool==inseq[i]) &
+#             (inseq[i]>0) & (inseq[i]<=ll) &
+#             (inseq[i-1]>0) & (inseq[i-1]<=ll) &
+#             !prevWasRep){
+#
+#           numer[inseq[i-1], inseq[i]-inseq[i-1]+ll] <- numer[inseq[i-1], inseq[i]-inseq[i-1]+ll]+1
+#           denom[inseq[i-1], pool-inseq[i-1]+ll] <- denom[inseq[i-1], pool-inseq[i-1]+ll]+1
+#
+#           pool <- setdiff(pool,inseq[i])
+#
+#         } else {
+#           if (!any(pool==inseq[i])){
+#             prevWasRep = TRUE
+#           } else {
+#             prevWasRep = FALSE
+#           }
+#         }
+#       }
+#     }
+#   }
+#   numer[ll] <- NA
+#   return(data.frame(lag=c(-(ll-1):0,1:(ll-1)),
+#                     lagrec=numer/denom))
+# }
 
 TCscoreTrial <- function(dat, ll, crit){
   # make sure repetitions have been excluded from the data
